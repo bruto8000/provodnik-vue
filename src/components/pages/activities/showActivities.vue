d43c35b6fdc7a7e6c0e2850ac07e80399d34213f
bruto8000 <54044706+bruto8000@users.noreply.github.com>
2021-05-17 01:48:48 +0300

    исправил фильтр в ShowActivities

--- src/components/pages/activities/showActivities.vue
+++ src/components/pages/activities/showActivities.vue
@@ -206,16 +206,11 @@
       style="padding-right:16px; border-bottom: 2px solid #26a69a; padding-bottom: 10px; margin-bottom: 5px;"
     >
       <div class="column is-1">
-        <input-date
-
-   :value.sync="filterSelect.fdate"
+        <input-date :value.sync="filterSelect.fdate"
         ></input-date>
       </div>
       <div class="column is-1">
-        <input-date
-      
-          
-        :value.sync="filterSelect.sdate"
+        <input-date :value.sync="filterSelect.sdate"
         ></input-date>
       </div>
       <div class="column is-1 p-1">
@@ -284,6 +279,8 @@
           <option value="Выполнено">Выполнено </option>
           <option value="Отложено">Отложено</option>
           <option value="Отменено">Отменено</option>
+          <option value="ДПП пассив">ДПП пассив</option>
+          <option value="ДПП актив">ДПП актив</option>
         </select>
       </div>
     </div>
@@ -410,34 +407,6 @@ this.needActivityModal = false;
       this.$store.commit("setDisplayingActivity", activity);
       this.needActivityModal = true;
     },
-    // exportToExcel: function() {
-    //   var tableToExcel = (function() {
-    //     var uri = "data:application/vnd.ms-excel;base64,",
-    //       template =
-    //         '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-    //       base64 = function(s) {
-    //         return window.btoa(unescape(encodeURIComponent(s)));
-    //       },
-    //       format = function(s, c) {
-    //         return s.replace(/{(\w+)}/g, function(m, p) {
-    //           return c[p];
-    //         });
-    //       };
-    //     return function(table, name) {
-    //       if (!table.nodeType) table = document.getElementById(table);
-    //       var ctx = {
-    //         worksheet: name || "Worksheet",
-    //         table: table.innerHTML,
-    //       };
-    //       window.location.href = uri + base64(format(template, ctx));
-    //     };
-    //   })();
-    //   this.table.created = true;
-    //   this.$nextTick(() => {
-    //     tableToExcel(document.getElementById("allTable"), "Активности");
-    //     this.table.created = false;
-    //   });
-    // },
     setQueryParams() {
       let urls = window.location.href.split("?");
       if (urls.length > 2) return;
